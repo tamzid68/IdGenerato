1e31a5d9b15c528af5af4d44f7d39e171d3001e2
ASM Tamzid <atkthegreat9999@gmail.com>
2024-09-21 23:31:02 +0600

    added model info

--- src/main/java/com/controller/IdGeneratoApplication.java
+++ src/main/java/com/controller/IdGeneratoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IdGeneratoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IdGeneratoApplication.class, args);
+		System.out.println("Sadia!");
 	}
 
 }
